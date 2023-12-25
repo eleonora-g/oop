311d3b532a1e9f69fc45aaeafd755dfd1ad7d795
eleonora-g <150892996+eleonora-g@users.noreply.github.com>
2023-12-25 23:04:39 +0700

    Add files via upload

---
+++ lab5/lab5/classes.java
@@ -0,0 +1,69 @@
+package lab5;
+import java.util.Scanner;
+
+public class classes {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int num1 = getInt();
+        int num2 = getInt();
+        char operation = getOperation();
+        if (operation == '/' && num2 == 0) {
+        	System.out.println("Невозможно выполнить деление на 0. Попробуйте еще раз.");
+            num1 = getInt();
+            num2 = getInt();
+            operation = getOperation();
+        }
+        int result = calc(num1,num2,operation);
+        System.out.println("Результат операции: "+result);
+    }
+
+    public static int getInt(){
+        System.out.println("Введите число:");
+        int num;
+        if(scanner.hasNextInt()){
+            num = scanner.nextInt();
+        } else {
+            System.out.println("Вы допустили ошибку при вводе числа. Попробуйте еще раз.");
+            scanner.next();
+            num = getInt();
+        }
+        return num;
+    }
+
+    public static char getOperation(){
+        System.out.println("Введите операцию:");
+        char operation;
+        if(scanner.hasNext()){
+            operation = scanner.next().charAt(0);
+        } else {
+            System.out.println("Вы допустили ошибку при вводе операции. Попробуйте еще раз.");
+            scanner.next();
+            operation = getOperation();
+        }
+        
+        return operation;
+    }
+    
+    public static int calc(int num1, int num2, char operation){
+        int result;
+        switch (operation){
+            case '+':
+                result = num1+num2;
+                break;
+            case '-':
+                result = num1-num2;
+                break;
+            case '*':
+                result = num1*num2;
+                break;
+            case '/':
+                result = num1/num2;
+                break;
+            default:
+                System.out.println("Операция не распознана. Повторите ввод.");
+                result = calc(num1, num2, getOperation());
+        }
+        return result;
+    }
+}
